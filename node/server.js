9fb548003f962ee36a31bd9dcbaaca11ebbd02da
張欽揚 <crazyms520@hotmail.com>
2017-05-29 23:29:02 +0800

    node

--- node/server.js
+++ node/server.js
@@ -1,16 +1,20 @@
 var http = require ('http');
+var url  = require('url');
 
-// http.createServer(function(request,response){
-//     response.writeHead(200,{"Content-Type":"text/plain"});
-//     response.write("Hello world");
-//     response.end();
-// }).listen(8888);
+function start(route){
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " recevied");
 
-function onRequest(request,response){
-    console.log("Request received");
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello world");
-    response.end();
+        route(pathname);
+
+        response.writeHead(200,{"Content-Type": "text/plain"});
+        response.write("Hello world");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
-http.createServer(onRequest).listen(8888);
-console.log("server is started");
+
+
+exports.start = start;
